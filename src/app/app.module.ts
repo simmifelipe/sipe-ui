f18a61b04cbabc983dbddf684c35edec6089fa8e
felipe.simmi <simmifelipe1992@gmail.com>
2018-09-14 13:12:36 -0300

    Criado o template do sistema e suas dependencias css e js

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MenubarComponent } from './menubar/menubar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MenubarComponent
   ],
   imports: [
     BrowserModule
