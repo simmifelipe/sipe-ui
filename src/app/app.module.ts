37fcd73739dd64ec831e845e0e0b302114b5d789
felipe.simmi <simmifelipe1992@gmail.com>
2018-09-15 14:19:12 -0300

    Iniciando a escolha da biblioteca de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MenubarComponent } from './menubar/menubar.component';
+import { UtilizadorCadastroComponent } from './utilizador/utilizador-cadastro/utilizador-cadastro.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    MenubarComponent
+    MenubarComponent,
+    UtilizadorCadastroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+
+    MatFormFieldModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/utilizador/utilizador-cadastro/utilizador-cadastro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-utilizador-cadastro',
+  templateUrl: './utilizador-cadastro.component.html',
+  styleUrls: ['./utilizador-cadastro.component.css']
+})
+export class UtilizadorCadastroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
